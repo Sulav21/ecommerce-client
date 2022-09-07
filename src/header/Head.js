e59256e8b9cd024f5d1e0d686df66ac7fed14a98
Sulav Adhikari <sulavadhikari17@gmail.com>
2022-09-07 21:17:52 +1000

    project initialized

---
+++ src/header/Head.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Head = () => {
+  return (
+    <>
+    <section className='head'>
+        <div className="container d_flex">
+            <div className="left row">
+                <i className='fa fa-phone'></i>
+            </div>
+        </div>
+    </section>
+    </>
+  )
+}
---
+++ src/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Head } from './Head'
+import { Navbar } from './Navbar'
+import { Search } from './Search'
+
+export const Header = () => {
+  return (
+    <>
+    <Head/>
+    <Search />
+    <Navbar />
+    </>
+  )
+}
