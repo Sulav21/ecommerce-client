0e4167bbb243c65d50d002475d96603bb80f31ad
Sulav Adhikari <sulavadhikari17@gmail.com>
2022-09-07 23:38:46 +1000

    search bar UI

--- src/header/Search.js
+++ src/header/Search.js
@@ -1,7 +1,28 @@
 import React from 'react'
-
+import logo from '../images/logo.svg'
+import { Link } from 'react-router-dom'
 export const Search = () => {
   return (
-    <div>Search</div>
+    <section className='search'>
+      <div className="container c_flex">
+        <div className="logo width">
+          <img src={logo} alt='' />
+        </div>
+        <div className="search-box f_flex">
+          <i className='fa fa-search'></i>
+          <input type="text" placeholder='Search products ...' />
+          <span>All Category</span>
+        </div>
+        <div className="icon f_flex width">
+     <i className='fa fa-user icon_circle'></i>
+     <div className="cart">
+      <Link to='/cart'>
+        <i className='fa fa-shopping-bag icon-circle'></i>
+        <span>0</span>
+      </Link>
+     </div>
+        </div>
+      </div>
+    </section>
   )
 }
